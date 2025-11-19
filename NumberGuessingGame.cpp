0aac2f1f5d604acc55b15cfa85fa022b20b1be8f
dumangasjohnlloyd <dumangasjohnlloyd@gmail.com>
2025-11-19 18:22:49 +0800

    feat(game): allow user to play after finishing

--- NumberGuessingGame.cpp
+++ NumberGuessingGame.cpp
@@ -10,59 +10,66 @@ int main() {
     //Seed the random number generator.
     srand(time(0));
 
-    //Generate a random number between 1 and 100
-    int secretNumber = rand() % 100 + 1;
-
-    int guess;
-    int attempts = 0;
     char playAgainSelection;
 
-    cout << "===\033[34m Number\033[0m \033[32mGuessing\033[0m \033[33mGame\033[0m ===" << endl;
-    cout << "I have selected a number between 1 and 100. Can you guess it?\n";
-    cout << "You have 10 attempts. \n \n";
+    while (true) {
+        //Generate a random number between 1 and 100
+        int secretNumber = rand() % 100 + 1;
+        int guess;
+        int attempts = 0;
 
-    //TODO: Use a loop to keep asking the player for guesses until they guess correctly
-    while (attempts < 10) {
-        cout << "Enter your guess: ";
-        if (!(cin >> guess)) {
-            handleInput();
+        cout << "\n===\033[34m Number\033[0m \033[32mGuessing\033[0m \033[33mGame\033[0m ===" << endl;
+        cout << "I have selected a number between 1 and 100. Can you guess it?\n";
+        cout << "You have 10 attempts. \n \n";
+
+        //TODO: Use a loop to keep asking the player for guesses until they guess correctly
+        while (attempts < 10) {
+            cout << "Enter your guess: ";
+            if (!(cin >> guess)) {
+                handleInput();
+                continue;
+            }
+
+            attempts++;
+
+            if(guess > secretNumber) {
+                cout << "\033[33m Too high! \033[0m\n";
+            } else if (guess < secretNumber) {
+                cout << "\033[34m Too low! \033[0m\n";
+            } else {
+                cout << "\033[32m Correct! You guessed it in " << attempts << " attempts. \033[0m \n \n";
+                break;
+            }
         }
 
-        attempts++;
+        //add output if the player ran out of attempts
 
-        if(guess > secretNumber) {
-            cout << "\033[33m Too high! \033[0m\n";
-        } else if (guess < secretNumber) {
-            cout << "\033[34m Too low! \033[0m\n";
+        while (true) {
+            cout << "Play again? ( y / n ): ";
+            cin >> playAgainSelection;
+
+            if (playAgainSelection == 'y' || playAgainSelection == 'n' ) {
+                break;
+            } else {
+                cout << "Invalid input. Please enter 'y' or 'n'.\n" << endl;
+            }
+        }
+
+        if(playAgainSelection == 'y') {
+            cout << "\nRestarting game...\n\n";
         } else {
-            cout << "\033[32m Correct! You guessed it in " << attempts << " attempts. \033[0m \n \n";
+            cout << "\nThanks for playing!\n";
             break;
         }
     }
 
-    cout << "Thanks for playing!" << endl << endl;
-
-    while (true) {
-        cout << "Play again? ( y / n ): ";
-        cin >> playAgainSelection;
-
-        if (playAgainSelection == 'y' || playAgainSelection == 'n' ) {
-            if(playAgainSelection == 'y') {
-                //restart the game
-            } else {
-                break;
-            }
-
-        } else {
-            cout << "Invalid input. Please enter 'y' or 'n'. " << endl;
-        }
-    }
+    
     return 0;
 }
 
 void handleInput() {
     cout << "\033[31m Invalid input! \033[0m";
-    cout << "Please enter a number: ";
+    cout << "Please enter a number.\n ";
     cin.clear();
     cin.ignore(1000, '\n');
 }
