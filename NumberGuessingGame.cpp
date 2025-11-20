0b987135ef7ebd4b21e4d452d34b7c815fafb6dd
dumangasjohnlloyd <dumangasjohnlloyd@gmail.com>
2025-11-20 15:36:10 +0800

    feat(game): add difficulty level option to number guessing game

--- NumberGuessingGame.cpp
+++ NumberGuessingGame.cpp
@@ -7,19 +7,52 @@ using namespace std;
 void handleInput();
 
 int main() {
-    //Seed the random number generator.
-    srand(time(0));
-
+    int difficulty;
+    int easy = 100, medium = 500, hard = 1000;
     char playAgainSelection;
+    int difficultySelection;
 
     while (true) {
+        cout << "\n===\033[34m Number\033[0m \033[32mGuessing\033[0m \033[33mGame\033[0m ===" << endl;
+        cout << "Select a difficulty:\n";
+        cout << "[1] Easy\n";
+        cout << "[2] Medium\n";
+        cout << "[3] Hard\n";
+        while (true) {
+            cout << "Enter your selection: ";
+            if (!(cin >> difficultySelection)) {
+                handleInput();
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        //add input handling when user tries to enter a value beyond the scope
+
+        switch (difficultySelection) {
+            case 1: 
+                difficulty = easy;
+                break;
+            
+            case 2:
+                difficulty = medium;
+                break;
+            
+            case 3: 
+                difficulty = hard;
+        }
+        
+        //Seed the random number generator.
+        srand(time(0));
+
         //Generate a random number between 1 and 100
-        int secretNumber = rand() % 100 + 1;
+        int secretNumber = rand() % difficulty + 1;
         int guess;
         int attempts = 0;
 
         cout << "\n===\033[34m Number\033[0m \033[32mGuessing\033[0m \033[33mGame\033[0m ===" << endl;
-        cout << "I have selected a number between 1 and 100. Can you guess it?\n";
+        cout << "I have selected a number between 1 and " << difficulty << ". Can you guess it?\n";
         cout << "You have 10 attempts. \n \n";
 
         //TODO: Use a loop to keep asking the player for guesses until they guess correctly
